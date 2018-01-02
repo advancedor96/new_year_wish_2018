87a73aa524efb80aa5200dbefbc8130a8608644f
advancedor96 <advancedor96@kfsyscc.org>
2018-01-03 00:46:02 +0800

    超過5字，多 ...

--- src/Msg.js
+++ src/Msg.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 
 const MSG = styled.span`
@@ -9,23 +9,56 @@ const MSG = styled.span`
 	align-items: center;
 	padding: 20px;
 	box-sizing: border-box;
+	cursor: pointer;
 `
 const CONTENT = styled.span`
 	font-size: 40px;
    font-weight: bold;
+	width: 100%;
+	text-overflow: ellipsis;
+	white-space: ${props=>props.expand? 'normal': 'nowrap'};
 `
 const NAME = styled.span`
 	border-bottom: 1px solid black;
 	font-size: 13px;
 	margin-top: 17px;
 `
-const Msg = (props)=>{
-	return (
-		<MSG bgColor={props.msg.bgColor}>
-			<CONTENT>{props.msg.msg}</CONTENT>
-			<NAME>{props.msg.name}</NAME>
-		</MSG>
-	)
+
+class Msg extends Component {
+	expand = ()=>{
+		const oldState = this.state.expand;
+		const updateState = !oldState;
+		this.setState({expand: updateState});
+	}
+	state = {
+		expand: false
+	}
+
+	render(){
+		let max_length = 5;
+		let show_content = this.props.msg.msg;
+		if(this.state.expand){
+			return (
+				<MSG bgColor={this.props.msg.bgColor} onClick={this.expand}>
+				<CONTENT expand>{show_content}</CONTENT>
+				<NAME>{this.props.msg.name}</NAME>
+			</MSG>
+			)
+		}
+		if(this.props.msg.msg.length >=max_length){
+			show_content = this.props.msg.msg.slice(0, max_length) + '...';
+
+		}
+		return (
+			<MSG bgColor={this.props.msg.bgColor} onClick={this.expand}>
+				<CONTENT>{show_content}</CONTENT>
+				<NAME>{this.props.msg.name}</NAME>
+			</MSG>
+		)
+	}
+
+
 }
+
 
 export default Msg;
